eb986069c649adef8b9c2ef416b5c20a288b9bc4
puifaiba <puifai.ba@gmail.com>
2021-04-01 11:24:05 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
+  state = {
+    renderDescription: null,
+  }
+
+  getRender = (renderDescription) => {
+    this.setState({renderDescription})
+
+    console.log(renderDescription)
+  }
+
+  componentDidMount = () => {}
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Counter />
+        <Header renderDescription={this.state.renderDescription} />
+        <Counter getRender={this.getRender} />
       </div>
-    );
+    )
   }
 }
 
@@ -20,28 +32,32 @@ class Header extends Component {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">
+          The current count is less than ${this.props.renderDescription}
+        </h1>
       </header>
-    );
+    )
   }
 }
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = {count: 0}
 
   increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+    this.setState({count: this.state.count + 1})
+    this.props.getRender(this.renderDescription())
+  }
 
   decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+    this.setState({count: this.state.count - 1})
+    this.props.getRender(this.renderDescription())
+  }
 
   renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
-  };
+    const remainder = this.state.count % 5
+    const upToNext = 5 - remainder
+    return this.state.count + upToNext
+  }
 
   render() {
     return (
@@ -49,10 +65,10 @@ class Counter extends Component {
         <h1>{this.state.count}</h1>
         <button onClick={this.decrement}> - </button>
         <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        {/* <h3>{this.props.getRender=() => this.renderDescription()}</h3> */}
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
